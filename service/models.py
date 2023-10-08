db5721bc4e5e2800ce7e40b094aeda9eb898c15f
JooooosephY <yangzhao7508@outlook.com>
2023-10-07 22:41:24 -0400

    add test_model and model, not finished

--- service/models.py
+++ service/models.py
@@ -4,7 +4,10 @@ Models for YourResourceModel
 All of the models are stored in this module
 """
 import logging
+from abc import abstractmethod
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+
 
 logger = logging.getLogger("flask.app")
 
@@ -14,12 +17,12 @@ db = SQLAlchemy()
 
 # Function to initialize the database
 def init_db(app):
-    """ Initializes the SQLAlchemy app """
-    YourResourceModel.init_db(app)
+    """Initializes the SQLAlchemy app"""
+    Order.init_db(app)
 
 
 class DataValidationError(Exception):
-    """ Used for an data validation errors when deserializing """
+    """Used for an data validation errors when deserializing"""
 
 
 class PersistentBase:
@@ -38,23 +41,23 @@ class PersistentBase:
 
     def create(self):
         """
-        Creates a Account to the database
+        Creates an Order to the database
         """
-        logger.info("Creating %s", self.name)
+        logger.info("Creating an order")
         self.id = None  # id must be none to generate next primary key
         db.session.add(self)
         db.session.commit()
 
     def update(self):
         """
-        Updates a Account to the database
+        Updates an Order to the database
         """
-        logger.info("Updating %s", self.name)
+        logger.info("Updating an Order %d", self.id)
         db.session.commit()
 
     def delete(self):
-        """Removes a Account from the data store"""
-        logger.info("Deleting %s", self.name)
+        """Removes an Order from the data store"""
+        logger.info("Deleting an Order %d", self.id)
         db.session.delete(self)
         db.session.commit()
 
@@ -81,94 +84,171 @@ class PersistentBase:
         return cls.query.get(by_id)
 
 
-class YourResourceModel(db.Model):
+class Item(db.Model, PersistentBase):
     """
-    Class that represents a YourResourceModel
+    Class that represents an Address
+    """
+
+    # Table Schema
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(
+        db.Integer, db.ForeignKey("order.id", ondelete="CASCADE"), nullable=False
+    )
+    name = db.Column(db.String(64))
+    price = db.Column(db.Double)
+    description = db.Column(db.String(128))
+    quantity = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"<Item {self.name} id=[{self.id}] order[{self.order_id}]>"
+
+    def __str__(self):
+        return f"{self.name}: {self.price}, {self.description}, {self.quantity}"
+
+    def serialize(self) -> dict:
+        """Converts an Address into a dictionary"""
+        return {
+            "id": self.id,
+            "order_id": self.order_id,
+            "name": self.name,
+            "price": self.price,
+            "description": self.description,
+            "quantity": self.quantity,
+        }
+
+    def deserialize(self, data: dict) -> None:
+        """
+        Populates an Address from a dictionary
+
+        Args:
+            data (dict): A dictionary containing the resource data
+        """
+        try:
+            self.order_id = data["order_id"]
+            self.name = data["name"]
+            self.price = data["price"]
+            self.description = data["description"]
+            self.quantity = data["quantity"]
+        except KeyError as error:
+            raise DataValidationError(
+                "Invalid Item: missing " + error.args[0]
+            ) from error
+        except TypeError as error:
+            raise DataValidationError(
+                "Invalid Item: body of request contained "
+                "bad or no data " + error.args[0]
+            ) from error
+        return self
+
+
+class Order(db.Model, PersistentBase):
+    """
+    Class that represents an Order
     """
 
     app = None
 
     # Table Schema
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(63))
+    customer_id = db.Column(db.Integer)
+    creation_time = db.Column(db.DateTime(), nullable=False, default=datetime.now())
+    last_updated_time = db.Column(db.DateTime(), nullable=False, default=datetime.now())
+    items = db.relationship("Item", backref="order", passive_deletes=True)
+    total_price = db.Column(db.Double)
 
     def __repr__(self):
-        return f"<YourResourceModel {self.name} id=[{self.id}]>"
-
-    def create(self):
-        """
-        Creates a YourResourceModel to the database
-        """
-        logger.info("Creating %s", self.name)
-        self.id = None  # pylint: disable=invalid-name
-        db.session.add(self)
-        db.session.commit()
-
-    def update(self):
-        """
-        Updates a YourResourceModel to the database
-        """
-        logger.info("Saving %s", self.name)
-        db.session.commit()
-
-    def delete(self):
-        """ Removes a YourResourceModel from the data store """
-        logger.info("Deleting %s", self.name)
-        db.session.delete(self)
-        db.session.commit()
+        return f"<Order from {self.customer_id} id=[{self.id}]>"
 
     def serialize(self):
-        """ Serializes a YourResourceModel into a dictionary """
-        return {"id": self.id, "name": self.name}
+        """Serializes an Order into a dictionary"""
+        order = {
+            "id": self.id,
+            "customer_id": self.customer_id,
+            "creation_time": self.creation_time.isoformat(),
+            "last_updated_time": self.last_updated_time.isoformat(),
+            "items": [],
+            "total_price": self.total_price,
+        }
+
+        for item in self.items:
+            order["items"].append(item.serialize())
+
+        return order
 
     def deserialize(self, data):
         """
-        Deserializes a YourResourceModel from a dictionary
+        Deserializes an Order from a dictionary
 
         Args:
             data (dict): A dictionary containing the resource data
         """
         try:
-            self.name = data["name"]
+            self.customer_id = data["customer_id"]
+            self.creation_time = datetime.fromisoformat(data["creation_time"])
+            self.last_updated_time = datetime.fromisoformat(data["last_updated_time"])
+            self.total_price = data["total_price"]
+            # handle inner list of addresses
+            item_list = data.get("items")
+            for json_item in item_list:
+                item = Item()
+                item.deserialize(json_item)
+                self.items.append(item)
+
         except KeyError as error:
             raise DataValidationError(
-                "Invalid YourResourceModel: missing " + error.args[0]
+                "Invalid Order: missing " + error.args[0]
             ) from error
         except TypeError as error:
             raise DataValidationError(
-                "Invalid YourResourceModel: body of request contained bad or no data - "
-                "Error message: " + error
+                "Invalid Order: body of request contained "
+                "bad or no data - " + error.args[0]
             ) from error
         return self
 
-    @classmethod
-    def init_db(cls, app):
-        """ Initializes the database session """
-        logger.info("Initializing database")
-        cls.app = app
-        # This is where we initialize SQLAlchemy from the Flask app
-        db.init_app(app)
-        app.app_context().push()
-        db.create_all()  # make our sqlalchemy tables
+    def get_total_price(self) -> float:
+        total = 0.0
+        for item in self.items:
+            total += item.price * float(item.quantity)
+        return total
+
+    def create(self):
+        """
+        Creates an Order to the database
+        """
+        logger.info("Creating an order")
+        self.id = None  # id must be none to generate next primary key
+
+        # Set the creation_time and last_updated_time as the time this function is called.
+        self.creation_time = datetime.now()
+        self.last_updated_time = self.creation_time
+
+        # Calculate the total_price for the order
+
+        self.total_price = self.get_total_price()
+
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        """
+        Updates an Order to the database
+        """
+        logger.info("Updating an Order %d", self.id)
+
+        # Set the last_updated_time as the time this function is called.
+        self.last_updated_time = datetime.now()
+
+        # Calculate the total_price for the order
+        self.total_price = self.get_total_price()
+
+        db.session.commit()
 
     @classmethod
-    def all(cls):
-        """ Returns all of the YourResourceModels in the database """
-        logger.info("Processing all YourResourceModels")
-        return cls.query.all()
-
-    @classmethod
-    def find(cls, by_id):
-        """ Finds a YourResourceModel by it's ID """
-        logger.info("Processing lookup for id %s ...", by_id)
-        return cls.query.get(by_id)
-
-    @classmethod
-    def find_by_name(cls, name):
-        """Returns all YourResourceModels with the given name
+    def find_by_customer_id(cls, customer_id):
+        """Returns all Orders with the given customer_id
 
         Args:
-            name (string): the name of the YourResourceModels you want to match
+            customer_id (integer): the customer_id of the Order you want to match
         """
-        logger.info("Processing name query for %s ...", name)
-        return cls.query.filter(cls.name == name)
+        logger.info("Processing customer_id query for %d ...", customer_id)
+        return cls.query.filter(cls.customer_id == customer_id)
--- tests/test_models.py
+++ tests/test_models.py
@@ -5,34 +5,281 @@ Test cases for YourResourceModel Model
 import os
 import logging
 import unittest
-from service.models import YourResourceModel, DataValidationError, db
+from service import app
+from service.models import Order, Item, DataValidationError, db
+from tests.factories import OrderFactory, ItemFactory
+
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
+)
 
 
 ######################################################################
 #  YourResourceModel   M O D E L   T E S T   C A S E S
 ######################################################################
 # pylint: disable=too-many-public-methods
-class TestYourResourceModel(unittest.TestCase):
-    """ Test Cases for YourResourceModel Model """
+class TestOrder(unittest.TestCase):
+    """Test Cases for YourOrder Model"""
 
     @classmethod
     def setUpClass(cls):
-        """ This runs once before the entire test suite """
+        """This runs once before the entire test suite"""
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        Order.init_db(app)
 
     @classmethod
     def tearDownClass(cls):
-        """ This runs once after the entire test suite """
+        """This runs once after the entire test suite"""
 
     def setUp(self):
-        """ This runs before each test """
+        """This runs before each test"""
+        db.session.query(Order).delete()  # clean up the last tests
+        db.session.query(Item).delete()  # clean up the last tests
+        db.session.commit()
 
     def tearDown(self):
-        """ This runs after each test """
+        """This runs after each test"""
+        db.session.remove()
 
     ######################################################################
     #  T E S T   C A S E S
     ######################################################################
 
-    def test_example_replace_this(self):
-        """ It should always be true """
-        self.assertTrue(True)
+    def test_create_an_order(self):
+        """It should Create an Order and assert that it exists"""
+        fake_order = OrderFactory()
+        # pylint: disable=unexpected-keyword-arg
+        order = Order(
+            customer_id=fake_order.customer_id,
+            creation_time=fake_order.creation_time,
+            last_updated_time=fake_order.last_updated_time,
+            total_price=fake_order.total_price,
+        )
+        self.assertIsNotNone(order)
+        self.assertEqual(order.id, None)
+        self.assertEqual(order.customer_id, fake_order.customer_id)
+        self.assertEqual(order.creation_time, fake_order.creation_time)
+        self.assertEqual(order.last_updated_time, fake_order.last_updated_time)
+        self.assertEqual(order.total_price, fake_order.total_price)
+
+    def test_add_an_order(self):
+        """It should Create an Order and add it to the database"""
+        orders = Order.all()
+        self.assertEqual(orders, [])
+        order = OrderFactory()
+        order.create()
+        # Assert that it was assigned an id and shows up in the database
+        self.assertIsNotNone(order.id)
+
+        orders = Order.all()
+        self.assertEqual(len(orders), 1)
+
+    def test_read_order(self):
+        """It should Read an Order"""
+        order = OrderFactory()
+        order.create()
+
+        # Read it back
+        found_order = Order.find(order.id)
+        self.assertEqual(found_order.id, order.id)
+        self.assertEqual(found_order.customer_id, order.customer_id)
+        self.assertEqual(found_order.creation_time, order.creation_time)
+        self.assertEqual(found_order.last_updated_time, order.last_updated_time)
+        self.assertEqual(found_order.total_price, order.total_price)
+        self.assertEqual(found_order.items, [])
+
+    def test_update_order(self):
+        """It should Update an Order"""
+        order = OrderFactory(customer_id=1)
+        order.create()
+        # Assert that it was assigned an id and shows up in the database
+        self.assertIsNotNone(order.id)
+        self.assertEqual(order.customer_id, 1)
+        self.assertEqual(order.creation_time, order.last_updated_time)
+
+        # Fetch it back
+        order = Order.find(order.id)
+        order.customer_id = 2
+        order.update()
+
+        # Fetch it back again, total_price would change and last_updated_time would be changed
+        order = Order.find(order.id)
+        self.assertEqual(order.customer_id, 2)
+        self.assertNotEqual(order.creation_time, order.last_updated_time)
+
+    def test_delete_an_order(self):
+        """It should Delete an Order from the database"""
+        orders = Order.all()
+        self.assertEqual(orders, [])
+        order = OrderFactory()
+        order.create()
+        # Assert that it was assigned an id and shows up in the database
+        self.assertIsNotNone(order.id)
+        orders = Order.all()
+        self.assertEqual(len(orders), 1)
+        order = orders[0]
+        order.delete()
+        orders = Order.all()
+        self.assertEqual(len(orders), 0)
+
+    def test_list_all_accounts(self):
+        """It should List all Orders in the database"""
+        orders = Order.all()
+        self.assertEqual(orders, [])
+        for order in OrderFactory.create_batch(5):
+            order.create()
+        # Assert that there are not 5 accounts in the database
+        orders = Order.all()
+        self.assertEqual(len(orders), 5)
+
+    def test_find_by_customer_id(self):
+        """It should Find an Order by customer_id"""
+        order = OrderFactory()
+        order.create()
+
+        # Fetch it back by name
+        same_account = Order.find_by_customer_id(order.customer_id)[0]
+        self.assertEqual(same_account.id, order.id)
+        self.assertEqual(same_account.customer_id, order.customer_id)
+
+    def test_serialize_an_order(self):
+        """It should Serialize an Order"""
+        order = OrderFactory()
+        item = ItemFactory()
+        order.items.append(item)
+        serial_order = order.serialize()
+        self.assertEqual(serial_order["id"], order.id)
+        self.assertEqual(serial_order["customer_id"], order.customer_id)
+        self.assertEqual(serial_order["creation_time"], order.creation_time.isoformat())
+        self.assertEqual(
+            serial_order["last_updated_time"], order.last_updated_time.isoformat()
+        )
+        self.assertEqual(serial_order["total_price"], order.total_price)
+        self.assertEqual(len(serial_order["items"]), 1)
+        items = serial_order["items"]
+        self.assertEqual(items[0]["id"], item.id)
+        self.assertEqual(items[0]["order_id"], item.order_id)
+        self.assertEqual(items[0]["name"], item.name)
+        self.assertEqual(items[0]["price"], item.price)
+        self.assertEqual(items[0]["description"], item.description)
+        self.assertEqual(items[0]["quantity"], item.quantity)
+
+    def test_deserialize_an_order(self):
+        """It should Deserialize an Order"""
+        order = OrderFactory()
+        order.items.append(ItemFactory())
+        order.create()
+        order = Order.find(order.id)
+        serial_order = order.serialize()
+        new_order = Order()
+        new_order.deserialize(serial_order)
+        self.assertEqual(new_order.customer_id, order.customer_id)
+        self.assertEqual(new_order.creation_time, order.creation_time)
+        self.assertEqual(new_order.last_updated_time, order.last_updated_time)
+        self.assertEqual(new_order.total_price, order.total_price)
+
+    def test_deserialize_order_with_key_error(self):
+        """It should not Deserialize an Order with a KeyError"""
+        order = Order()
+        self.assertRaises(DataValidationError, order.deserialize, {})
+
+    def test_deserialize_order_with_type_error(self):
+        """It should not Deserialize an Order with a TypeError"""
+        order = Order()
+        self.assertRaises(DataValidationError, order.deserialize, [])
+
+    def test_deserialize_item_key_error(self):
+        """It should not Deserialize an Item with a KeyError"""
+        item = Item()
+        self.assertRaises(DataValidationError, item.deserialize, {})
+
+    def test_deserialize_item_type_error(self):
+        """It should not Deserialize an Item with a TypeError"""
+        item = Item()
+        self.assertRaises(DataValidationError, item.deserialize, [])
+
+    def test_add_order_item(self):
+        """It should Create an account with an address and add it to the database"""
+        orders = Order.all()
+        self.assertEqual(orders, [])
+        order = OrderFactory()
+
+        item = ItemFactory()
+        order.items.append(item)
+
+        order.create()
+        # Assert that it was assigned an id and shows up in the database
+        self.assertIsNotNone(order.id)
+        orders = Order.all()
+        self.assertEqual(len(orders), 1)
+        self.assertEqual(len(order.items), 1)
+        self.assertEqual(order.items[0].price, item.price)
+        self.assertEqual(order.items[0].quantity, item.quantity)
+        self.assertEqual(order.total_price, item.price * item.quantity)
+
+        new_order = Order.find(order.id)
+        self.assertEqual(new_order.items[0].name, item.name)
+
+        item2 = ItemFactory()
+        order.items.append(item2)
+        order.update()
+
+        new_order = Order.find(order.id)
+        self.assertEqual(len(new_order.items), 2)
+        self.assertEqual(new_order.items[1].name, item2.name)
+        self.assertEqual(
+            order.total_price, item.price * item.quantity + item2.price * item2.quantity
+        )
+
+    # def test_update_account_address(self):
+    #     """It should Update an accounts address"""
+    #     accounts = Account.all()
+    #     self.assertEqual(accounts, [])
+
+    #     account = AccountFactory()
+    #     address = AddressFactory(account=account)
+    #     account.create()
+    #     # Assert that it was assigned an id and shows up in the database
+    #     self.assertIsNotNone(account.id)
+    #     accounts = Account.all()
+    #     self.assertEqual(len(accounts), 1)
+
+    #     # Fetch it back
+    #     account = Account.find(account.id)
+    #     old_address = account.addresses[0]
+    #     print("%r", old_address)
+    #     self.assertEqual(old_address.city, address.city)
+    #     # Change the city
+    #     old_address.city = "XX"
+    #     account.update()
+
+    #     # Fetch it back again
+    #     account = Account.find(account.id)
+    #     address = account.addresses[0]
+    #     self.assertEqual(address.city, "XX")
+
+    # def test_delete_account_address(self):
+    #     """It should Delete an accounts address"""
+    #     accounts = Account.all()
+    #     self.assertEqual(accounts, [])
+
+    #     account = AccountFactory()
+    #     address = AddressFactory(account=account)
+    #     account.create()
+    #     # Assert that it was assigned an id and shows up in the database
+    #     self.assertIsNotNone(account.id)
+    #     accounts = Account.all()
+    #     self.assertEqual(len(accounts), 1)
+
+    #     # Fetch it back
+    #     account = Account.find(account.id)
+    #     address = account.addresses[0]
+    #     address.delete()
+    #     account.update()
+
+    #     # Fetch it back again
+    #     account = Account.find(account.id)
+    #     self.assertEqual(len(account.addresses), 0)
