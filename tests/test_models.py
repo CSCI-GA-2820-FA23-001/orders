23b26e032358fa46c50d7bf8fc7dd0fa5ee3faf9
Matthew Dong <matthew.dong@nyu.edu>
2023-11-10 22:18:26 -0500

    tester comment

--- tests/test_models.py
+++ tests/test_models.py
@@ -23,8 +23,8 @@ class TestOrder(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        """This runs once before the entire test suite"""
-        app.config["TESTING"] = True
+        """This runs once before the entire test suite""" #comment
+        app.config["TESTING"] = True 
         app.config["DEBUG"] = False
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
